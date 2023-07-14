245268a1e6d3a3c04c41e0cb99ec68838abc2d7d
Eamon Dang <eamondang@gmail.com>
2023-07-14 20:57:01 +0700

    initial: exercise bootcamp

--- src/lib.rs
+++ src/lib.rs
@@ -5,33 +5,30 @@ use near_sdk::near_bindgen;
 // Define the contract structure
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize)]
-pub struct Contract {
-  count: u32,
+pub struct Contract {}
+
+// Outsourcing or E-Commerce
+pub trait ImplementOutsourcing {
+  fn create_user();
+  fn take_job();
+  fn create_client();
+  fn create_job();
+  fn view_job_by_id();
+  fn view_all_jobs_per_client();
+  fn view_all_jobs();
+  fn payment(); // Payment -> Jobs will remove from list
 }
 
-// Define the default, which automatically initializes the contract
-impl Default for Contract {
-  fn default() -> Self {
-    Self { count: 0 }
-  }
+// -> Thứ 3 tuần sau
+pub trait ImplementECommerce {
+  fn create_shop();
+  fn create_product();
+  fn view_all_products();
+  fn view_all_products_per_shop();
+  fn view_product_by_id();
+  fn payment(); // Payment -> Product decrement total_supply;
 }
 
 // Implement the contract structure
 #[near_bindgen]
-impl Contract {
-  // Public method - Get the current count
-  pub fn get_number(&self) -> u32 {
-    self.count
-  }
-
-  // Call this method to increment the count by a given number
-  pub fn plus(&mut self, number: u32) {
-    self.count += number;
-  }
-
-  // Private method
-  #[private]
-  fn plus_one(&mut self) {
-    self.count += 1;
-  }
-}
+impl Contract {}
