45900c97f44921d422718da44f84c9906f167e77
Nguyễn Ngọc Sơn <ngocson741852963@gmail.com>
2023-07-18 17:31:26 +0700

    complete

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,46 @@
 #![allow(dead_code)]
+pub mod metadata;
+pub mod ecommerce;
+
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::near_bindgen;
+use near_sdk::{near_bindgen, AccountId, PanicOnDefault, env};
+use near_sdk::collections::{UnorderedMap};
+
+use metadata::{ShopMetadata, Product};
 
 // Define the contract structure
 #[near_bindgen]
-#[derive(BorshDeserialize, BorshSerialize)]
-pub struct Contract {}
-
-// Outsourcing or E-Commerce
-pub trait ImplementOutsourcing {
-  fn create_user();
-  fn take_job();
-  fn create_client();
-  fn create_job();
-  fn view_job_by_id();
-  fn view_all_jobs_per_client();
-  fn view_all_jobs();
-  fn payment(); // Payment -> Jobs will remove from list
+#[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
+pub struct Contract {
+  owner_id: AccountId,
+  shops: UnorderedMap<AccountId, ShopMetadata>,
+  products: UnorderedMap<String, Product>,
 }
 
-// -> Thứ 3 tuần sau
-pub trait ImplementECommerce {
-  fn create_shop();
-  fn create_product();
-  fn view_all_products();
-  fn view_all_products_per_shop();
-  fn view_product_by_id();
-  fn payment(); // Payment -> Product decrement total_supply;
-}
+// Define the default, which automatically initializes the contract
 
 // Implement the contract structure
 #[near_bindgen]
-impl Contract {}
+impl Contract {
+    #[init]
+    pub fn new() -> Self {
+        Self {
+            owner_id: env::signer_account_id(),   
+            shops: UnorderedMap::new(b"s".to_vec()),
+            products: UnorderedMap::new(b"p".to_vec()),
+        }
+    }
+
+    pub fn view_all_shop (&self) -> Vec<ShopMetadata>{
+        self.shops.values().collect()
+    }
+
+    pub fn insert_product(&mut self, product_id: String, amount: u64){
+        let mut product = self.products.get(&product_id).expect("Product does not exist");
+        let shop = self.shops.get(&product.shop_id).expect("Shop does not exist");
+        assert!(shop.owner == env::signer_account_id(),"you are not the shop owner");
+
+        product.total_supply += amount;
+        self.products.insert(&product_id, &product);
+    }
+}
