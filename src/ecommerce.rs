996c54286e4d45deb300558acdea7f2f28e10776
Nguyễn Ngọc Sơn <ngocson741852963@gmail.com>
2023-07-18 22:43:45 +0700

    update_shop and update_product

--- src/ecommerce.rs
+++ src/ecommerce.rs
@@ -13,6 +13,8 @@ pub trait ImplementECommerce {
   fn view_all_products(&self) -> Vec<Product>;
   fn view_all_products_per_shop(&self, shop_id: AccountId) -> Vec<Product>;
   fn view_product_by_id(&self, product_id: String) -> Option<Product>;
+  fn update_shop(&mut self,shop_id: AccountId, name: String, decription: String) -> ShopMetadata;
+  fn update_product(&mut self, product_id: String, shop_id: AccountId, name: String, price: Balance, total_supply: u64) ->Product;
   fn payment(&mut self, product_id: String, amount: u64) -> Promise;
 }
 
@@ -52,16 +54,35 @@ impl ImplementECommerce for Contract {
     self.products.get(&product_id)
   }
 
+  fn update_shop(&mut self,shop_id: AccountId, name: String, decription: String) -> ShopMetadata {
+    let shop = self.shops.get(&shop_id).expect("Shop does not exist");
+    assert!(shop.owner.eq(&env::signer_account_id()),"you are not the shop owner");
+
+    let new_shop = ShopMetadata {shop_id, name, decription, owner: shop.owner };
+    self.shops.insert(&shop.shop_id, &new_shop);
+    new_shop
+  }
+
+  fn update_product(&mut self, product_id: String, shop_id: AccountId, name: String, price: Balance, total_supply: u64) ->Product {
+    let product = self.products.get(&product_id).expect("Product does not exist");
+    let owner = self.shops.get(&product.shop_id).expect("Shop does not exist").owner;
+    assert!(owner.eq(&env::signer_account_id()),"you are not the shop owner");
+
+    let new_product = Product { product_id: product_id.clone(), shop_id, name, price, total_supply };
+    self.products.insert(&product_id, &new_product);
+    new_product
+  }
+
   #[payable]
   fn payment(&mut self, product_id: String, amount: u64) -> Promise {
     let mut product = self.products.get(&product_id).expect("Product does not exist");
-    assert!(product.total_supply > 0, "Product out of stock");
-    let price = product.price * amount as u128;
-    assert!(price <= env::attached_deposit(), "Not equal the price");
+    assert!(product.total_supply > amount, "Product out of stock");
+    let price = (product.price * amount as u128) * 10u128.pow(24);
+    assert!(price == env::attached_deposit(), "Not equal the price");
 
     product.total_supply -= amount;
     self.products.insert(&product_id, &product);
     let owner = self.shops.get(&product.shop_id).expect("Product does not exist");
-    Promise::new(owner.owner).transfer(price)
+    Promise::new(owner.owner.clone()).transfer(price)
   }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
 #![allow(dead_code)]
-pub mod metadata;
 pub mod ecommerce;
+pub mod metadata;
 
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{near_bindgen, AccountId, PanicOnDefault, env};
-use near_sdk::collections::{UnorderedMap};
+use near_sdk::collections::UnorderedMap;
+use near_sdk::{env, near_bindgen, AccountId, PanicOnDefault};
 
-use metadata::{ShopMetadata, Product};
+use metadata::{Product, ShopMetadata};
 
 // Define the contract structure
 #[near_bindgen]
@@ -22,25 +22,26 @@ pub struct Contract {
 // Implement the contract structure
 #[near_bindgen]
 impl Contract {
-    #[init]
-    pub fn new() -> Self {
-        Self {
-            owner_id: env::signer_account_id(),   
-            shops: UnorderedMap::new(b"s".to_vec()),
-            products: UnorderedMap::new(b"p".to_vec()),
-        }
+  #[init]
+  pub fn new() -> Self {
+    Self {
+      owner_id: env::signer_account_id(),
+      shops: UnorderedMap::new(b"s".to_vec()),
+      products: UnorderedMap::new(b"p".to_vec()),
     }
+  }
 
-    pub fn view_all_shop (&self) -> Vec<ShopMetadata>{
-        self.shops.values().collect()
-    }
+  pub fn view_all_shop(&self) -> Vec<ShopMetadata> {
+    self.shops.values().collect()
+  }
 
-    pub fn insert_product(&mut self, product_id: String, amount: u64){
-        let mut product = self.products.get(&product_id).expect("Product does not exist");
-        let shop = self.shops.get(&product.shop_id).expect("Shop does not exist");
-        assert!(shop.owner == env::signer_account_id(),"you are not the shop owner");
+  pub fn app_product_total_supply(&mut self, product_id: String, amount: u64) {
+    let mut product = self.products.get(&product_id).expect("Product does not exist");
+    let shop = self.shops.get(&product.shop_id).expect("Shop does not exist");
+    assert!(shop.owner == env::signer_account_id(), "you are not the shop owner");
 
-        product.total_supply += amount;
-        self.products.insert(&product_id, &product);
-    }
+    product.total_supply += amount;
+    self.products.insert(&product_id, &product);
+  }
+
 }
